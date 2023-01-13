73e7c904e7830a733e18f9fc9f766fc67a15828b
samuelhfalci <samuelhfalci@gmail.com>
2023-01-13 19:59:16 -0300

    staging ta ok

--- api/recipes/index.ts
+++ api/recipes/index.ts
@@ -1,9 +1,13 @@
 import { RecipeDatabase } from "../../src/recipes/recipe-database";
 import { allowCors } from "../../src/utils/allow-cors";
 const handler = (req: any, res: any) => {
+  const { start, length } = req.query;
   const recipeDatabase = new RecipeDatabase();
-  const recipes = recipeDatabase.find();
-  res.status(200).json(recipes);
+  let recipes;
+  if (start && length) {
+    recipes = recipeDatabase.find({ skip: +start, take: +length });
+  } else recipes = recipeDatabase.find();
+  res.status(200).json({ recordsTotal: recipeDatabase.count(), data: recipes });
 };
 
 export default allowCors(handler);
