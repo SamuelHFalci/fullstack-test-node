4242621c4031d770a44b5e1dae313e9102f1b393
samuelhfalci <samuelhfalci@gmail.com>
2023-01-13 19:57:57 -0300

    allow cors

--- api/recipes/index.ts
+++ api/recipes/index.ts
@@ -1,6 +1,9 @@
 import { RecipeDatabase } from "../../src/recipes/recipe-database";
-export default function handler(req: any, res: any) {
+import { allowCors } from "../../src/utils/allow-cors";
+const handler = (req: any, res: any) => {
   const recipeDatabase = new RecipeDatabase();
   const recipes = recipeDatabase.find();
   res.status(200).json(recipes);
-}
+};
+
+export default allowCors(handler);
