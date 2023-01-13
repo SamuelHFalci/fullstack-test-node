ad9ffda0d458b07638d465d2798bc0f9ed5ccc60
samuelhfalci <samuelhfalci@gmail.com>
2023-01-13 20:13:50 -0300

    ajustando main

--- api/recipes/index.ts
+++ api/recipes/index.ts
@@ -1,6 +1,8 @@
 import { RecipeDatabase } from "../../src/recipes/recipe-database";
-export default function handler(req: any, res: any) {
+import { allowCors } from "../../src/utils/allow-cors";
+const handler = (req: any, res: any) => {
   const recipeDatabase = new RecipeDatabase();
   const recipes = recipeDatabase.find();
-  res.status(200).json(recipes);
-}
+  res.status(200).json({ recordsTotal: recipeDatabase.count(), data: recipes });
+};
+export default allowCors(handler);
